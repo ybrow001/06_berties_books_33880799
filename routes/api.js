e6e0d8b604b263f83b2ffd135d2c82f9eb5bf136
Yew Brown <brown.voss32@gmail.com>
2025-12-04 17:54:20 +0000

    added price betwen min-max book api search

--- routes/api.js
+++ routes/api.js
@@ -56,5 +56,55 @@ router.get('/books', function (req, res, next) {
     })
 });
 
+router.get('/books/price', function (req, res, next) {
+    // query database to get books
+
+    let min = req.query.min;
+    let max = req.query.max;
+
+
+    if(!min || !max) {
+        let sqlquery = "SELECT * FROM books"
+
+        db.query(sqlquery, (err, result) => {
+        // Return results as a JSON object
+            if(err) {
+                res.json(err);
+                next(err)
+            } else {
+                res.json(result)
+            }
+        });
+        return
+    }
+    // not needed due to requiring html inputs but kept in as a fallback
+
+    min = min.trim();
+    max = max.trim();
+
+    let sqlquery = "SELECT name, price FROM books WHERE price BETWEEN ? AND ?";
+    db.query(sqlquery, [min, max], (err, result) => {
+        // return results as a JSON object
+        if(err) {
+            res.json(err);
+            next(err)
+        } else if(result.length === 0){
+            let sqlquery = "SELECT * FROM books"
+
+            db.query(sqlquery, (err2, allBooks) => {
+                // Return results as a JSON object
+                if(err) {
+                    res.json(err2);
+                    next(err2)
+                } else {
+                    res.json(allBooks)
+                }
+            })
+        } else {
+            res.json(result)
+        }
+    })
+});
+
 // export the router object so index.js can access it
 module.exports = router;
